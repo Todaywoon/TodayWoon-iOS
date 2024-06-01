9565eb2002f0584e21aa4d07a927a21a62331100
YuJeong <qordbwjd96@gmail.com>
2024-06-02 02:30:01 +0900

    [feat] Camera 개발 기능

--- TodayWoon/Sources/Presentation/Home/HomeViewController.swift
+++ TodayWoon/Sources/Presentation/Home/HomeViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class HomeViewController: UIViewController {
     
@@ -19,6 +20,9 @@ class HomeViewController: UIViewController {
         button.isHidden = true
         return button
     }()
+    
+    private let imageView = UIImageView()
+    private var photoImage = UIImage()
     
     override func viewDidLoad() {
         view.backgroundColor = .white
@@ -43,6 +47,12 @@ class HomeViewController: UIViewController {
             make.top.equalTo(walkStartButton.snp.bottom).offset(8)
         }
         walkEndButton.addTarget(self, action: #selector(walkEndButtonClicked), for: .touchUpInside)
+        
+        self.view.addSubview(imageView)
+        imageView.snp.makeConstraints { make in
+            make.width.height.equalTo(100)
+            make.center.equalToSuperview()
+        }
     }
     
     @objc private func walkStartButtonClicked() {
@@ -52,7 +62,61 @@ class HomeViewController: UIViewController {
             walkEndButton.isHidden = false
         } else {
             /// CameraVC 로 이동
+            openCamera()
         }
+    }
+    
+    private func openCamera() {
+      #if targetEnvironment(simulator)
+      fatalError()
+      #endif
+      // Privacy - Camera Usage Description
+      AVCaptureDevice.requestAccess(for: .video) { [weak self] isAuthorized in
+        guard isAuthorized else {
+          self?.showAlertGoToSetting()
+          return
+        }
+        
+        DispatchQueue.main.async {
+          let pickerController = UIImagePickerController() // must be used from main thread only
+          pickerController.sourceType = .camera
+          pickerController.allowsEditing = false
+          pickerController.mediaTypes = ["public.image"]
+          // 만약 비디오가 필요한 경우,
+    //      imagePicker.mediaTypes = ["public.movie"]
+    //      imagePicker.videoQuality = .typeHigh
+          pickerController.delegate = self
+          self?.present(pickerController, animated: true)
+        }
+      }
+    }
+    
+    func showAlertGoToSetting() {
+      let alertController = UIAlertController(
+        title: "현재 카메라 사용에 대한 접근 권한이 없습니다.",
+        message: "설정 > hangahanga 탭 에서 접근을 활성화 할 수 있습니다.",
+        preferredStyle: .alert
+      )
+      let cancelAlert = UIAlertAction(
+        title: "취소",
+        style: .cancel
+      ) { _ in
+          alertController.dismiss(animated: true, completion: nil)
+        }
+      let goToSettingAlert = UIAlertAction(
+        title: "설정으로 이동하기",
+        style: .default) { _ in
+          guard
+            let settingURL = URL(string: UIApplication.openSettingsURLString),
+            UIApplication.shared.canOpenURL(settingURL)
+          else { return }
+          UIApplication.shared.open(settingURL, options: [:])
+        }
+      [cancelAlert, goToSettingAlert]
+        .forEach(alertController.addAction(_:))
+      DispatchQueue.main.async {
+        self.present(alertController, animated: true) // must be used from main thread only
+      }
     }
     
     @objc private func walkEndButtonClicked() {
@@ -63,3 +127,19 @@ class HomeViewController: UIViewController {
         }
     }
 }
+
+extension HomeViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            picker.dismiss(animated: true)
+            return
+        }
+        
+        self.imageView.image = image
+        
+        // 업로드 로직 넣어야함
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+  }

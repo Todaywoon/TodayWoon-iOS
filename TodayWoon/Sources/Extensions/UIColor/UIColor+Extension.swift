cb6bc30359cb3df0320d1e44e8a3e10286c50e0a
mumu <tnddls2ek@naver.com>
2024-06-02 01:48:58 +0900

    [feat] color extension 추가

---
+++ TodayWoon/Sources/Extensions/UIColor/UIColor+Extension.swift
@@ -0,0 +1,26 @@
+//
+//  UIColor+Extension.swift
+//  TodayWoon
+//
+//  Created by 이숭인 on 6/2/24.
+//
+
+import UIKit
+
+extension UIColor {
+    static let gray50 = UIColor(named: "gray50")
+    static let gray100 = UIColor(named: "gray100")
+    static let gray200 = UIColor(named: "gray200")
+    static let gray300 = UIColor(named: "gray300")
+    static let gray400 = UIColor(named: "gray400")
+    static let gray500 = UIColor(named: "gray500")
+    static let gray600 = UIColor(named: "gray600")
+    static let gray700 = UIColor(named: "gray700")
+    static let gray800 = UIColor(named: "gray800")
+    static let primary = UIColor(named: "primary")
+    static let primary2 = UIColor(named: "primary2")
+    static let primary3 = UIColor(named: "primary3")
+    static let black100 = UIColor(named: "black")
+    static let red100 = UIColor(named: "red100")
+    
+}
